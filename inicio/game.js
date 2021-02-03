b55f709553474b0b90c21eb10bb3f0d6a486232a
Alberto Muñoz <alberto.munozfuertes@salesianos.edu>
2021-02-03 10:22:57 +0100

    First Commit: - Skeleton - Resources - Guide

---
+++ inicio/game.js
@@ -0,0 +1,12 @@
+/** 
+ * Juego creado por: Alumnos de Grado Superior de DAM
+ * Licencia: GPL
+*/
+$(document).ready(function() {
+    /** COMPLETAR EN CLASE */
+   
+    /** ACCIÓN PLAY GAME */
+
+    /** ACCIÓN MUSICA */
+}
+);
