d91b66868f871e70c18bfa21bd996f3eda060c6c
Alberto Muñoz <71640301+AlbertoSalesianos@users.noreply.github.com>
2021-02-03 10:33:08 +0100

    Update game.js

--- inicio/game.js
+++ inicio/game.js
@@ -1,12 +1,18 @@
 /** 
- * Juego creado por: Alumnos de Grado Superior de DAM
+ * Juego creado por: Alumnos de Grado Superior de DAM Salesianos Zaragoza
  * Licencia: GPL
 */
 $(document).ready(function() {
     /** COMPLETAR EN CLASE */
    
-    /** ACCIÓN PLAY GAME */
+    /** TODO: IMPLEMENTAR ACCIÓN PLAY GAME al pulsar 
+     * el botton LOCAL o ONLINE
+     * Se oculta los elementos visibles al cargar y se muestra 
+     * un fondo negro
+     */
 
-    /** ACCIÓN MUSICA */
+    /** TODO: FUNCIÓN PARA REPRODUCIR MUSICA 
+     * usando el elemento html5 audio
+    */
 }
 );
